c7532db1a66032c65d02c0618da8552e3e3270c2
yoowonsuk <sam2yuk@naver.com>
2019-08-03 22:55:00 +0900

    3rd Aug 2019

---
+++ ch1/01-1/Five12Print.java
@@ -0,0 +1,9 @@
+class Five12Print{
+  public static void main(String[] args){
+    System.out.println(12);
+    System.out.println("1"+2);
+    System.out.println(1+"2");
+    System.out.println("12");
+    System.out.println("1"+"2");
+  }
+}
---
+++ ch1/FirstJavaProgram.java
@@ -0,0 +1,6 @@
+class FirstJavaProgram{
+  public static void main(String[] args){
+    System.out.println("Welcome to Java");
+    System.out.println("First Java program");
+  }
+}
